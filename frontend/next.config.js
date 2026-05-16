f7090eefba6cd722e369ffc9138fa2d048252d39
zaheer453512 <alhijrahcollege20@gmail.com>
2026-05-16 23:11:18 +0500

    hello gandu

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,11 +3,6 @@ const nextConfig = {
   images: {
     domains: ['res.cloudinary.com'],
   },
-  env: {
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
-    NEXT_PUBLIC_WHATSAPP: process.env.NEXT_PUBLIC_WHATSAPP,
-    NEXT_PUBLIC_EMAIL: process.env.NEXT_PUBLIC_EMAIL,
-  },
 };
 
 module.exports = nextConfig;
